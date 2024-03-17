e340d5239ebd8d1d21b90c4f152748a7d71a0e49
ashrafali2000 <ali4282271@gmail.com>
2024-03-17 15:08:33 +0500

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const DbPassword = process.env.USER_PASSWORD;
 mongoose
   .connect(
-    `mongodb+srv://ashraf:${DbPassword}@mern-blog-app.kxs0srn.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog-app`
+    `mongodb+srv://ashraf:${DbPassword}@mern-blog-app.kxs0srn.mongodb.net/mern-blog-app?retryWrites=true&w=majority&appName=mern-blog-app`
   )
   .then(() => {
     console.log("DataBase is connected--->");
@@ -16,8 +17,10 @@ mongoose
     console.log("err------>", err);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
