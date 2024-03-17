91f0c8c9e4f7091488015fa07bedb257d7ffcd48
ashrafali2000 <ali4282271@gmail.com>
2024-03-17 06:33:17 +0500

    add MongoDb Connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const DbPassword = process.env.USER_PASSWORD;
+mongoose
+  .connect(
+    `mongodb+srv://ashraf:${DbPassword}@mern-blog-app.kxs0srn.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog-app`
+  )
+  .then(() => {
+    console.log("DataBase is connected--->");
+  })
+  .catch((err) => {
+    console.log("err------>", err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
