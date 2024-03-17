f47a2361325e906b80e556cab0411fbb7da63554
ashrafali2000 <ali4282271@gmail.com>
2024-03-17 16:13:37 +0500

    create signup ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
       <form>
         <TextInput
           type="text"
-          placeholder="search..."
+          placeholder="Search..."
           rightIcon={RiSearchLine}
           className="hidden lg:inline"
         />
