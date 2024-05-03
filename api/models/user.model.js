0369b6d928d80c7a10f22a80eb50f5ebc68f6853
ashrafali2000 <ali4282271@gmail.com>
2024-05-03 11:20:47 +0500

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "/profileimg.webp",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
