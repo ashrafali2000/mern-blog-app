fc13706255687fab2b5ee7808e6a02465fce7cea
ashrafali2000 <ali4282271@gmail.com>
2024-05-05 22:25:13 +0500

    create post pai route

---
+++ api/models/post.model.js
@@ -0,0 +1,35 @@
+import { mongoose } from "mongoose";
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default: "/blog.png",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
