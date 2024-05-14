f3fe61f565259c66159143fb7fcbe0a4b4651664
ashrafali2000 <ali4282271@gmail.com>
2024-05-14 22:26:10 +0500

    complete the backend setup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 
 dotenv.config();
 const DbPassword = process.env.USER_PASSWORD;
@@ -20,7 +20,7 @@ mongoose
   .catch((err) => {
     console.log("err------>", err);
   });
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -33,12 +33,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
+
+app.get("/", function (req, res) {
+  res.send("Server is running...");
 });
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
