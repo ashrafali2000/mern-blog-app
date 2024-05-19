43dcce9c6117da32f794c65b18c6ac70a3b7a486
ashrafali2000 <ali4282271@gmail.com>
2024-05-19 20:40:12 +0500

    commet cookieparser

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import cors from "cors";
 // import path from "path";
 
@@ -30,7 +30,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 });
