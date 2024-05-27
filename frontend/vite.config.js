78e2a84913036da7b3dac1ad3dc8ae0a836676e8
ashrafali2000 <ali4282271@gmail.com>
2024-05-27 09:51:46 +0500

    update url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://mern-blog-app-one.vercel.app",
-  //       changeOrigin: true,
-  //       secure: true,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://mern-blog-app-one.vercel.app/",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600 },
 });
