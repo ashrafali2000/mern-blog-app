5ce6ed6639ab523c15a100603680f27c27e2eca9
ashrafali2000 <ali4282271@gmail.com>
2024-05-27 10:34:13 +0500

    add production

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,8 +51,7 @@ export const signin = async (req, res, next) => {
       .status(200)
       .cookie("access_token", token, {
         // httpOnly: true,
-        secure: true,
-        // secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent only over HTTPS
+        secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent only over HTTPS
         // sameSite: "strict", // Helps mitigate CSR
       })
       .json(rest);
