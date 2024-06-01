1dbd1909e61f5b6c5868a1c607eec95c5f7cf4af
ashrafali2000 <ali4282271@gmail.com>
2024-06-01 06:50:39 +0500

    update httponly to false

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -50,16 +50,11 @@ export const signin = async (req, res, next) => {
     res
       .status(200)
       .cookie("access_token", token, {
-        // // httpOnly: true,
-        // secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent only over HTTPS
-        // // sameSite: "strict", // Helps mitigate CSR
-        // expires: new Date(new Date().getTime() + 30 + 1000),
         path: "/",
-        httpOnly: true,
+        httpOnly: false,
         expires: new Date(Date.now() + 900000),
         sameSite: "none",
         secure: true,
-        // domain: "https://mern-blog-app-py3d.vercel.app/",
       })
       .json(rest);
   } catch (error) {
