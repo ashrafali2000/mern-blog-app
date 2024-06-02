991704012c18e2d2e90e34899e839b91cdc69e29
ashrafali2000 <ali4282271@gmail.com>
2024-06-02 16:01:19 +0500

    check the error

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -3,11 +3,11 @@ import { errorHandler } from "./error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(errorHandler(401, "Unauthorized"));
+    return next(errorHandler(401, "Unauthorized----1"));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return next(errorHandler(401, "Unauthorized"));
+      return next(errorHandler(401, "Unauthorized----2"));
     }
     req.user = user;
     next();
